f5344149f968a8384914a762681e49867d8c2e9d
OrazioLentini <oraziio@hotmail.com>
2013-04-08 17:11:56 -0400

    April 8 2013

---
+++ April 8 2013/js/main.js
@@ -0,0 +1,116 @@
+window.HomeView = Backbone.View.extend({
+
+    template:_.template($('#home').html()),
+
+    render:function (eventName) {
+        $(this.el).html(this.template());
+        return this;
+    }
+});
+
+window.Page1View = Backbone.View.extend({
+
+    template:_.template($('#canon').html()),
+
+    render:function (eventName) {
+        $(this.el).html(this.template());
+        return this;
+    }
+});
+
+window.Page2View = Backbone.View.extend({
+
+    template:_.template($('#nikon').html()),
+
+    render:function (eventName) {
+        $(this.el).html(this.template());
+        return this;
+    }
+});
+
+window.Page3View = Backbone.View.extend({
+
+    template:_.template($('#sony').html()),
+
+    render:function (eventName) {
+        $(this.el).html(this.template());
+        return this;
+    }
+});
+
+window.Page4View = Backbone.View.extend({
+
+    template:_.template($('#about').html()),
+
+    render:function (eventName) {
+        $(this.el).html(this.template());
+        return this;
+    }
+});
+
+var AppRouter = Backbone.Router.extend({
+
+    routes:{
+        "":"home",
+        "canon":"canon",
+        "nikon":"nikon",
+        "sony":"sony",
+        "about":"about"
+
+    },
+
+    initialize:function () {
+        // Handle back button throughout the application
+        $('.back').live('click', function(event) {
+            window.history.back();
+            return false;
+        });
+        this.firstPage = true;
+    },
+
+    home:function () {
+        console.log('#home');
+        this.changePage(new HomeView());
+    },
+
+    canon:function () {
+        console.log('#canon');
+        this.changePage(new Page1View());
+    },
+
+    nikon:function () {
+        console.log('#nikon');
+        this.changePage(new Page2View());
+    },
+    
+    sony:function () {
+        console.log('#sony');
+        this.changePage(new Page3View());
+    },
+    
+    about:function () {
+        console.log('#about');
+        this.changePage(new Page4View());
+    },
+
+
+    changePage:function (page) {
+        $(page.el).attr('data-role', 'page');
+        page.render();
+        $('body').append($(page.el));
+        var transition = $.mobile.defaultPageTransition;
+        // We don't want to slide the first page
+        if (this.firstPage) {
+            transition = 'none';
+            this.firstPage = false;
+        }
+        $.mobile.changePage($(page.el), {changeHash:false, transition: transition});
+    }
+
+});
+
+$(document).ready(function () {
+    console.log('document ready');
+    app = new AppRouter();
+    Backbone.history.start();
+});
