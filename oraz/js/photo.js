4d82731a85a19e4a59f924c3376ed3787e898cb7
OrazioLentini <oraziio@hotmail.com>
2013-04-24 21:00:02 -0400

    search pictures and models

--- oraz/js/photo.js
+++ oraz/js/photo.js
@@ -1,5 +1,73 @@
 var apiKey = "cbb61449315fa2eb1cd8cda781f00b40";
 
+function getModels(str){
+  s = str;
+  //alert(s);
+  $.ajax({
+    url: "http://api.flickr.com/services/rest/?",
+    data: {
+      api_key: apiKey,
+      format: 'json',
+      method: "flickr.cameras.getBrandModels",
+      brand: s,
+    },
+    dataType: 'jsonp',
+    jsonp: 'jsoncallback',
+    success: function(rsp){
+      window.rsp = rsp;
+      console.log(rsp);
+      $("#cameraModels ul").empty();
+      for (var i=0; i<10; i++) {
+          camera = rsp.cameras.camera[i];
+          var brand = rsp.cameras.brand;
+          var model = camera.id;
+          var image = camera.images.small;
+          //var cameraImage="js/bypass.php?image_src="+image;
+          console.log(image);
+          s = brand + " " + model;
+          //alert(s);
+          $("#cameraModels ul").append("<li><a onclick='getPhotos(s)' href=#results><img class='ui-li-icon' src='"+image+"'>"+brand+"  "+model+"</a></li>");
+          $("#cameraModels ul").listview('refresh');  
+      }
+    }
+  });
+}
+
+function getPhotos(s){
+  //s = str;
+  $.ajax({
+      url: "http://api.flickr.com/services/rest/?",
+      data: {
+        api_key: apiKey,
+        format: 'json',
+        method: "flickr.photos.search",
+        tags: s,
+        per_page: 30
+      },
+      dataType: 'jsonp',
+      jsonp: 'jsoncallback',
+      success: function(rsp){
+        window.rsp = rsp;
+        console.log(rsp);
+        var output='';
+        for (var i=0; i<rsp.photos.photo.length; i++) {
+          photo = rsp.photos.photo[i];
+          var title = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "q.jpg";
+          var link = "http://www.flickr.com/photos/" + photo.owner + "/" + photo.id;
+          var blocktype = ((i%3)===2) ? 'c':
+                          ((i%3)===1) ? 'b':
+                          'a';
+          output += '<div class="ui-block-' + blocktype + '">';
+          output += '<a href="#showphoto" data-transition="fade" onclick="showPhoto(\'' + link + '\', \'' + title + '\')">';
+          output += '<a href="' + link + '">' + '<img alt="'+ photo.title + '"src="' + title + '"/>' + '</a>';
+          output += '</a>';
+          output += '</div>';
+        } // go through each photo
+        $('#photolist').html(output);
+      }
+    });
+}
+/*
 $(document).ready(function(){
    $("#searchButton").click(function(){
    s = $("input#searchphoto").val();
@@ -10,10 +78,12 @@ $(document).ready(function(){
          api_key: apiKey,
          format: 'json',
          method: "flickr.cameras.getBrandModels",
-         brand: s
+         brand: s,
+         data: 'data'
         },
      });
   });
+ 
 });
  
 function jsonFlickrApi(rsp) {
@@ -25,11 +95,33 @@ function jsonFlickrApi(rsp) {
             var brand = rsp.cameras.brand;
             var model = camera.id;
             var image = camera.images.small;
-            var cameraImage="image_src="+image;
+            //var cameraImage="js/bypass.php?image_src="+image;
             console.log(image);
-            $("#cameraModels ul").append("<li><a href=#results><img class='ui-li-icon' src='"+cameraImage+"'>"+brand+"  "+model+"</a></li>");
+            $("#cameraModels ul").append("<li><a onclick='json' href=#results><img class='ui-li-icon' src='"+image+"'>"+brand+"  "+model+"</a></li>");
             $("#cameraModels ul").listview('refresh');
         } // go through each photo
       //  $('#cameraModels').html(output);
 }
 
+
+function jsonFlickrApi1(rsp) {
+    window.rsp = rsp;
+    console.log(rsp);
+    var output='';
+    for (var i=0; i<rsp.photos.photo.length; i++) {
+            photo = rsp.photos.photo[i];
+            var title = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "q.jpg";
+            var link = "http://www.flickr.com/photos/" + photo.owner + "/" + photo.id;
+            var blocktype = ((i%3)===2) ? 'c':
+                            ((i%3)===1) ? 'b':
+                            'a';
+            output += '<div class="ui-block-' + blocktype + '">';
+            output += '<a href="#showphoto" data-transition="fade" onclick="showPhoto(\'' + link + '\', \'' + title + '\')">';
+            output += '<a href="' + link + '">' + '<img alt="'+ photo.title + '"src="' + title + '"/>' + '</a>';
+            output += '</a>';
+            output += '</div>';
+    } // go through each photo
+    $('#photolist').html(output);
+}
+
+*/
