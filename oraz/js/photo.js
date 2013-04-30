4d042ffa7e6a5c1d403c24bed0a1fe288ef04edd
Orazio Lentini <oraziio@hotmail.com>
2013-04-29 23:45:23 -0300

    edited image size

--- oraz/js/photo.js
+++ oraz/js/photo.js
@@ -32,6 +32,7 @@ function getModels(str){
 
 function getPhotos(model){
   s = model;
+  saveModel(s);
   //alert(model);
   $.ajax({
       url: "http://api.flickr.com/services/rest/?",
@@ -51,76 +52,43 @@ function getPhotos(model){
         var head='';
         for (var i=0; i<rsp.photos.photo.length; i++) {
           photo = rsp.photos.photo[i];
-          var title = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "q.jpg";
+          var title = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "z.jpg";
           var link = "http://www.flickr.com/photos/" + photo.owner + "/" + photo.id;
           var blocktype = ((i%3)===2) ? 'c':
                           ((i%3)===1) ? 'b':
                           'a';
-          output += '<div class="ui-block-' + blocktype + '">';
+          //output += '<div class="ui-block-' + blocktype + '">';
+          output += '<div class="ui-block-' + 'a' + '">';
           output += '<a href="#showphoto" data-transition="fade" onclick="showPhoto(\'' + link + '\', \'' + title + '\')">';
           output += '<a href="' + link + '">' + '<img alt="'+ photo.title + '"src="' + title + '"/>' + '</a>';
           output += '</a>';
           output += '</div>';
         } // go through each photo
         $('#photolist').html(output);
+        
       }
     });
 }
-/*
-$(document).ready(function(){
-   $("#searchButton").click(function(){
-   s = $("input#searchphoto").val();
-     //alert(s);
-     $.ajax({
-       url: "http://api.flickr.com/services/rest/?",
-       data: {
-         api_key: apiKey,
-         format: 'json',
-         method: "flickr.cameras.getBrandModels",
-         brand: s,
-         data: 'data'
-        },
-     });
-  });
- 
-});
- 
-function jsonFlickrApi(rsp) {
-        window.rsp = rsp;
-        console.log(rsp);
-        $("#cameraModels ul").empty();
-        for (var i=0; i<10; i++) {
-            camera = rsp.cameras.camera[i];
-            var brand = rsp.cameras.brand;
-            var model = camera.id;
-            var image = camera.images.small;
-            //var cameraImage="js/bypass.php?image_src="+image;
-            console.log(image);
-            $("#cameraModels ul").append("<li><a onclick='json' href=#results><img class='ui-li-icon' src='"+image+"'>"+brand+"  "+model+"</a></li>");
-            $("#cameraModels ul").listview('refresh');
-        } // go through each photo
-      //  $('#cameraModels').html(output);
+function saveModel(s)
+{
+  console.log(s);
+	localStorage.setItem('favModel', s);	
 }
 
-
-function jsonFlickrApi1(rsp) {
-    window.rsp = rsp;
-    console.log(rsp);
-    var output='';
-    for (var i=0; i<rsp.photos.photo.length; i++) {
-            photo = rsp.photos.photo[i];
-            var title = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "q.jpg";
-            var link = "http://www.flickr.com/photos/" + photo.owner + "/" + photo.id;
-            var blocktype = ((i%3)===2) ? 'c':
-                            ((i%3)===1) ? 'b':
-                            'a';
-            output += '<div class="ui-block-' + blocktype + '">';
-            output += '<a href="#showphoto" data-transition="fade" onclick="showPhoto(\'' + link + '\', \'' + title + '\')">';
-            output += '<a href="' + link + '">' + '<img alt="'+ photo.title + '"src="' + title + '"/>' + '</a>';
-            output += '</a>';
-            output += '</div>';
-    } // go through each photo
-    $('#photolist').html(output);
+function addFavs(){
+  favM = localStorage.getItem("favModel");
+  //alert(favM);
+  console.log(favM);
+  $("#fav ul").empty();
+  $("#fav ul").append("<li><a onclick=getPhotos('"+favM+"') href=#results><img class='ui-li-icon'>"+favM+"</a></li>");
+  $("#fav ul").listview('refresh'); 
+}
+/*
+function showPhoto(link, title) {
+	var output='<a href="#results" data-transition="fade">';
+	output += '<img src="' + link + '_b.jpg" alt="' +title + '" />';
+	output += '</a>';
+	$('#myphoto').html(output);
 }
 
 */
