48f3f8b0dfca52c7eea90f2a8762a8270a06dfa3
Orazio Lentini <oraziio@hotmail.com>
2013-05-01 17:00:22 -0300

    fixed local storage

--- oraz/js/photo.js
+++ oraz/js/photo.js
@@ -17,13 +17,14 @@ function getModels(str){
       window.rsp = rsp;
       console.log(rsp);
       $("#cameraModels ul").empty();
-      for (var i=0; i<10; i++) {
+      for (var i=0; i<15; i++) {
           camera = rsp.cameras.camera[i];
           var brand = rsp.cameras.brand;
           var model = camera.id;
           var image = camera.images.small;
           //var cameraImage="js/bypass.php?image_src="+image;
-          $("#cameraModels ul").append("<li><a onclick=getPhotos('"+model+"') href=#results><img class='ui-li-icon' src='"+image+"'>"+brand+" "+model+"</a></li>");
+          //$("#cameraModels ul").append("<li><a onclick=getPhotos('"+model+"') href=#results><img class='ui-li-icon' src='"+cameraImage+"'>"+brand+" "+model+"</a></li>");
+	  $("#cameraModels ul").append("<li><a onclick=getPhotos('"+ brand +"_" +model+"') href=#results>"+brand+" "+model+"</a></li>");
           $("#cameraModels ul").listview('refresh');  
       }
     }
@@ -33,7 +34,7 @@ function getModels(str){
 function getPhotos(model){
   s = model;
   saveModel(s);
-  //alert(model);
+  //alert(s);
   $.ajax({
       url: "http://api.flickr.com/services/rest/?",
       data: {
@@ -54,13 +55,14 @@ function getPhotos(model){
           photo = rsp.photos.photo[i];
           var title = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "z.jpg";
           var link = "http://www.flickr.com/photos/" + photo.owner + "/" + photo.id;
-          var blocktype = ((i%3)===2) ? 'c':
+         /* var blocktype = ((i%3)===2) ? 'c':
                           ((i%3)===1) ? 'b':
-                          'a';
+                          'a'; */
           //output += '<div class="ui-block-' + blocktype + '">';
           output += '<div class="ui-block-' + 'a' + '">';
-          output += '<a href="#showphoto" data-transition="fade" onclick="showPhoto(\'' + link + '\', \'' + title + '\')">';
-          output += '<a href="' + link + '">' + '<img alt="'+ photo.title + '"src="' + title + '"/>' + '</a>';
+          ///output += '<a data-transition="fade" onclick="showPhoto(\'' + link + '\', \'' + title + '\')">';
+          //output += '<a href="' + link + '">' + '<img alt="'+ photo.title + '"src="' + title + '"/>' + '</a>';
+	  output += '<img alt="'+ photo.title + '"src="' + title + '"/>' + '</a>';
           output += '</a>';
           output += '</div>';
         } // go through each photo
@@ -69,26 +71,34 @@ function getPhotos(model){
       }
     });
 }
-function saveModel(s)
-{
-  console.log(s);
-	localStorage.setItem('favModel', s);	
+function saveModel(s){
+	console.log(s);
+	sessionStorage.setItem('favModel', s);	
 }
 
 function addFavs(){
-  favM = localStorage.getItem("favModel");
-  //alert(favM);
-  console.log(favM);
-  $("#fav ul").empty();
-  $("#fav ul").append("<li><a onclick=getPhotos('"+favM+"') href=#results><img class='ui-li-icon'>"+favM+"</a></li>");
-  $("#fav ul").listview('refresh'); 
-}
-/*
-function showPhoto(link, title) {
-	var output='<a href="#results" data-transition="fade">';
-	output += '<img src="' + link + '_b.jpg" alt="' +title + '" />';
-	output += '</a>';
-	$('#myphoto').html(output);
+  favM = sessionStorage.getItem("favModel");
+  alert('Added to Favorites.');
+
+  var favModels = JSON.parse(localStorage.getItem("key")) || [];
+  favModels.push(favM);
+  localStorage.setItem('key', JSON.stringify(favModels));
+  
+  //console.log(JSON.parse(localStorage.getItem("key")));
 }
 
-*/
+
+function loadFavs() {
+  console.log(JSON.parse(localStorage.getItem("key")));
+  var array = JSON.parse(localStorage.getItem("key"));
+  //alert(array[5]);
+ 
+  $("#fav ul").empty();
+  for (var i=0; i<array.length; i++) {
+    var favCam = array[i];
+    $("#fav ul").append("<li><a onclick=getPhotos('"+favCam+"') href=#results>"+favCam+"</a></li>");
+    $("#fav ul").listview('refresh');  
+  }
+
+}
+
