37c0f6d7158cbe43d75c4c500cb3937e2da9df61
Orazio Lentini <oraziio@hotmail.com>
2013-05-04 21:05:15 -0300

    fixed back to homepage

--- oraz/js/photo.js
+++ oraz/js/photo.js
@@ -2,6 +2,7 @@ var apiKey = "cbb61449315fa2eb1cd8cda781f00b40";
 
 function getModels(str){
   s = str;
+  saveBrand(s);
   //alert(s);
   $.ajax({
     url: "http://api.flickr.com/services/rest/?",
@@ -56,13 +57,7 @@ function getPhotos(model){
           photo = rsp.photos.photo[i];
           var title = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "z.jpg";
           var link = "http://www.flickr.com/photos/" + photo.owner + "/" + photo.id;
-         /* var blocktype = ((i%3)===2) ? 'c':
-                          ((i%3)===1) ? 'b':
-                          'a'; */
-          //output += '<div class="ui-block-' + blocktype + '">';
           output += '<div class="ui-block-' + 'a' + '">';
-          ///output += '<a data-transition="fade" onclick="showPhoto(\'' + link + '\', \'' + title + '\')">';
-          //output += '<a href="' + link + '">' + '<img alt="'+ photo.title + '"src="' + title + '"/>' + '</a>';
 	  output += '<img alt="'+ photo.title + '"src="' + title + '"/>' + '</a>';
           output += '</a>';
           output += '</div>';
@@ -73,6 +68,8 @@ function getPhotos(model){
     });
 }
 
+// FOR FAVORITES --------------------------------------------------------------------------------------------------
+
 function saveModel(s){
 	console.log(s);
 	sessionStorage.setItem('favModel', s);	
@@ -80,7 +77,7 @@ function saveModel(s){
 
 function addFavs(){
   favM = sessionStorage.getItem("favModel");
-  alert('Added to Favorites.'); 
+  alert('Added to Favorites.');
 
   var favModels = JSON.parse(localStorage.getItem("key")) || [];
   favModels.push(favM);
@@ -97,7 +94,7 @@ function loadFavs() {
   $("#fav ul").empty();
   for (var i=0; i<array.length; i++) {
     var favCam = array[i];
-    $("#fav ul").append("<li><a onclick=getPhotos('"+favCam+"') href=#results>"+favCam+"</a></li>");
+    $("#fav ul").append("<li><a onclick=getPhotos('"+favCam+"') href=#favResults>"+favCam+"</a></li>");
     $("#fav ul").listview('refresh');  
   }
 }
@@ -105,5 +102,48 @@ function loadFavs() {
 function clearFavs(){
   localStorage.clear();
   location.reload();
+}
+
+function removeFav() {
+  
+}
+
+
+// RELOAD MODELS--------------------------------------------------------------------------------------------
+
+function saveBrand(s){
+	console.log(s);
+	sessionStorage.setItem('saveBrand', s);	
+}
+
+function reloadModels(){
+  s = sessionStorage.getItem("saveBrand");
+  //alert(s);
+  $.ajax({
+    url: "http://api.flickr.com/services/rest/?",
+    data: {
+      api_key: apiKey,
+      format: 'json',
+      method: "flickr.cameras.getBrandModels",
+      brand: s,
+    },
+    dataType: 'jsonp',
+    jsonp: 'jsoncallback',
+    success: function(rsp){
+      window.rsp = rsp;
+      console.log(rsp);
+      $("#cameraModels ul").empty();
+      for (var i=0; i<15; i++) {
+          camera = rsp.cameras.camera[i];
+          var brand = rsp.cameras.brand;
+          var model = camera.id;
+          var image = camera.images.small;
+          //var cameraImage="js/bypass.php?image_src="+image;
+          //$("#cameraModels ul").append("<li><a onclick=getPhotos('"+model+"') href=#results><img class='ui-li-icon' src='"+cameraImage+"'>"+brand+" "+model+"</a></li>");
+	  $("#cameraModels ul").append("<li><a onclick=getPhotos('"+ brand + "_" +model+"') href=#results>"+brand+" "+model+"</a></li>");
+          $("#cameraModels ul").listview('refresh');  
+      }
+    }
+  });
 }
 
